823763c2b371636e663f19cec438f255d123ae87
DerPicknicker <louislol882@gmail.com>
2026-01-14 18:16:50 +0100

    Enable iOS Control Center for AudioUnit-based playback
    - Added beginReceivingRemoteControlEvents() in iOSApp.swift (required for apps using AudioUnit instead of AVPlayer)
    - Added UIBackgroundModes: audio to Info.plist (required for background audio and Control Center)

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import ComposeApp
+import UIKit
 
 @main
 struct iOSApp: App {
@@ -9,6 +10,10 @@ struct iOSApp: App {
     init() {
         // Register the Swift implementation with Kotlin
         PlatformPlayerProvider.shared.player = player
+        
+        // Required for apps using AudioUnit (like MPV) to appear in Control Center
+        // Must be called for remote control events to work with non-AVPlayer audio
+        UIApplication.shared.beginReceivingRemoteControlEvents()
     }
 
     var body: some Scene {
